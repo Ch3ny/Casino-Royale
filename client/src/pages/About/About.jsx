ed94a4e9b0b59990ba8bbf23a6938c1daa3aee20
Tomsatomsic <stepan.tomsa@seznam.cz>
2024-06-03 15:20:02 +0200

    init

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -2,17 +2,34 @@ import "./About.css";
 import { Link } from "react-router-dom";
 
 export default function About() {
-    (
+    return (
         <>
-        <div className="content">
+        
             
-        </div>
-        <Link to={"/"}>
        
-<button className="back" role="button">Go back</button>
+        <div className="content">
+       
+<b>We are MonkeyGangCasino</b><br></br>
+<p>
+Do you like old school slot machines and other gambling games ? If so our casino might just be your cup of tea.<br></br>
+In our casino you can choose from 4 games that you can play. In each game theres high stakes but also a high chance to win big<br></br>
+
+<b>Roulette</b><br></br>
+Old school classic roulette with high stakes and a unique design.<br></br>
+
+<b>Guess the number</b><br></br>
 
 
-      </Link>
+<b>Slot machine</b><br></br>
+
+
+<b>Scratch cards</b>
+
+
+</p>
+</div>
+
+     
         
         
         </>
